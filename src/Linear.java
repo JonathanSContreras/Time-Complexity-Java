540ac90a5126dbb01c7b9ba855c114bddfb07cf3
Jonathan Contreras <jonathancontreras0103@gmail.com>
2023-09-14 09:47:01 -0500

    quadratic time

--- src/Linear.java
+++ src/Linear.java
@@ -1,7 +1,7 @@
 public class Linear {
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5};
-        String[] names = {"Jonathan", "Brinanna", "Xander"};
+        String[] names = {"Jonathan", "Brianna", "Xander"};
         // linear1(arr);
         // linear2(arr);
         linear3(arr, names);
--- src/QuadraticTime.java
+++ src/QuadraticTime.java
@@ -1,3 +1,26 @@
 public class QuadraticTime {
-
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        String[] names = {"Jonathan", "Brianna", "Xander"};
+        log1(arr);
+        log2(arr);
+    }
+    public static void log1(int[] numbers){
+        // O(n)
+        for (int first:numbers) //outer loop
+            // O(n)
+            for (int second:numbers) //inner loop
+                System.out.println(first +","+ second);
+        // runtime complexity of the method would be O(n*n) -> O(n^2)
+    }
+    public static void log2(int[] numbers){
+        // O(n)
+        for (int first:numbers) //outer loop
+            // O(n)
+            for (int second:numbers) //inner loop
+                // O(n)
+                for (int third:numbers) //inner loop
+                    System.out.println(first +","+ second +","+ third);
+        // runtime complexity of the method would be O(n*n*n) -> O(n^3)
+    }
 }
